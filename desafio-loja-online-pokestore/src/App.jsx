b116bd294e51b034efe59dce9fb981a2238d7bfc
wjuniori <wjuniori.si@gmail.com>
2020-07-05 11:16:35 -0300

    feat: nav development

---
+++ desafio-loja-online-pokestore/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav className="topnav">
+        <div className="container">
+          <div className="topnav__content">
+            <h1 className="topnav__title">Pokestore</h1>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default App;
