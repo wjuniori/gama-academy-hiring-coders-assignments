08d8eeb9ed34b2994d8f8eb12ff3e33c133a3815
wjuniori <wjuniori.si@gmail.com>
2020-07-05 11:21:49 -0300

    feat: products section development

--- desafio-loja-online-pokestore/src/App.jsx
+++ desafio-loja-online-pokestore/src/App.jsx
@@ -11,6 +11,93 @@ function App() {
           </div>
         </div>
       </nav>
+      <main>
+        <div className="container">
+          <div className="row">
+            <section className="products">
+              <article className="product">
+                <div className="product__header">
+                  <img
+                    className="product__image"
+                    src="https://pokeres.bastionbot.org/images/pokemon/1.png"
+                    alt="Imagem do Pokémon Bulbasaur"
+                  />
+                </div>
+                <div className="product__body">
+                  <p>bulbasaur</p>
+                  <h3>R$ 30,00</h3>
+                </div>
+              </article>
+              <article className="product">
+                <div className="product__header">
+                  <img
+                    className="product__image"
+                    src="https://pokeres.bastionbot.org/images/pokemon/1.png"
+                    alt="Imagem do Pokémon Bulbasaur"
+                  />
+                </div>
+                <div className="product__body">
+                  <p>bulbasaur</p>
+                  <h3>R$ 30,00</h3>
+                </div>
+              </article>
+              <article className="product">
+                <div className="product__header">
+                  <img
+                    className="product__image"
+                    src="https://pokeres.bastionbot.org/images/pokemon/1.png"
+                    alt="Imagem do Pokémon Bulbasaur"
+                  />
+                </div>
+                <div className="product__body">
+                  <p>bulbasaur</p>
+                  <h3>R$ 30,00</h3>
+                </div>
+              </article>
+              <article className="product">
+                <div className="product__header">
+                  <img
+                    className="product__image"
+                    src="https://pokeres.bastionbot.org/images/pokemon/1.png"
+                    alt="Imagem do Pokémon Bulbasaur"
+                  />
+                </div>
+                <div className="product__body">
+                  <p>bulbasaur</p>
+                  <h3>R$ 30,00</h3>
+                </div>
+              </article>
+              <article className="product">
+                <div className="product__header">
+                  <img
+                    className="product__image"
+                    src="https://pokeres.bastionbot.org/images/pokemon/1.png"
+                    alt="Imagem do Pokémon Bulbasaur"
+                  />
+                </div>
+                <div className="product__body">
+                  <p>bulbasaur</p>
+                  <h3>R$ 30,00</h3>
+                </div>
+              </article>
+              <article className="product">
+                <div className="product__header">
+                  <img
+                    className="product__image"
+                    src="https://pokeres.bastionbot.org/images/pokemon/1.png"
+                    alt="Imagem do Pokémon Bulbasaur"
+                  />
+                </div>
+                <div className="product__body">
+                  <p>bulbasaur</p>
+                  <h3>R$ 30,00</h3>
+                </div>
+              </article>
+            </section>
+            <section className="shopping">Segunda Coluna</section>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
